503a64246838434a742bfb5d1aa036c202f1adc2
jimuelcaratao <caratao.jimuel@gmail.com>
2021-04-01 14:20:48 +0800

    fixing workspace

--- resources/views/layouts/normal_user.blade.php
+++ resources/views/layouts/normal_user.blade.php
@@ -19,22 +19,25 @@
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased bg-gray-100">
 
+        {{-- Main navbar for normal users --}}
+        @livewire('normal-navigation')
 
-            {{-- <!-- Page Heading -->
+            <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endif --}}
+            @endif
+
             <!-- Page Content -->
-            
+
             <main>
                 {{ $slot }}
             </main>
-
+        @livewireScripts
     </body>
 </html>
