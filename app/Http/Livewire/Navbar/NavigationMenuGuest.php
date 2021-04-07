e76c4ef00124a099686faa80ab35102d9d8585bb
Gabriel Cacayan <gabrielcacayan@gmail.com>
2021-04-07 17:41:48 +0800

    Finished working on navbar dropdown.

---
+++ app/Http/Livewire/Navbar/NavigationMenuGuest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Navbar;
+
+use Livewire\Component;
+
+class NavigationMenuGuest extends Component
+{
+    public function render()
+    {
+        return view('livewire.navbar.navigation-menu-guest');
+    }
+}
