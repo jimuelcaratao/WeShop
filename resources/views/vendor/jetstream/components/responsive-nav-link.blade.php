628b49a3713ae3655cbae0b2635b622b4c8cb617
Gabriel Cacayan <gabrielcacayan@gmail.com>
2021-04-17 16:10:30 +0800

    Updated navbar

--- resources/views/vendor/jetstream/components/responsive-nav-link.blade.php
+++ resources/views/vendor/jetstream/components/responsive-nav-link.blade.php
@@ -6,7 +6,7 @@
 //             : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
 
 $classes = ($active ?? false)
-            ? 'block pl-3 pr-4 py-2 border-l-4 border-green-300 text-base font-medium text-green-500 bg-green-50 focus:outline-none focus:text-green-600 focus:bg-green-100 focus:border-green-300 transition duration-150 ease-in-out'
+            ? 'block pl-3 pr-4 py-2 border-l-4 border-green-500 text-base font-medium text-green-700 bg-green-50 focus:outline-none focus:text-green-700 focus:bg-green-100 focus:border-green-500 transition duration-150 ease-in-out'
             : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-300 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
