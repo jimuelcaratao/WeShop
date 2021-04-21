99f72cffe713a053ac1df89ddb4970c9f4d1b4ab
HJ Miranda <hjmiranda02@gmail.com>
2021-04-21 17:31:02 +0800

    initial commit

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,13 @@
-@extends('errors::minimal')
+@extends('errors::illustrated-layout') 
 
 @section('title', __('Server Error'))
 @section('code', '500')
-@section('message', __('Server Error'))
+@section('message', __('Internal Server Error'))
+@section('subMessage', __("We are working towards creating something better."))
+@section('subMessage2', __("Won't be long..."))
+
+@section('image')
+ <img src="{{ asset('images/server.svg') }}" style="width: 80%;" alt="500 error">
+@endsection
+
+
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,10 @@
-@extends('errors::minimal')
+@extends('errors::illustrated-layout') 
 
 @section('title', __('Service Unavailable'))
 @section('code', '503')
 @section('message', __('Service Unavailable'))
+@section('subMessage', __("Don't worry though, there is always a way to go back home."))
+
+@section('image')
+ <img src="{{ asset('images/notavailable.svg') }}" style="width: 80%;" alt="503 error">
+@endsection
