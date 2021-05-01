33670ec7ec7cb970aae71665ce25d2cd75ba4216
Gabriel Cacayan <gabrielcacayan@gmail.com>
2021-05-01 20:01:17 +0800

    Created single product page

---
+++ resources/views/Pages/NormalUser/product.blade.php
@@ -0,0 +1,51 @@
+<x-normal_user>
+
+    <div class="bg-white p-5 my-12 w-11/12 mx-auto flex flex-col justify-center items-center">
+        {{-- For product's show-off --}}
+        <div class="p-5 w-4/5 border-b border-gray-400 flex flex-col md:flex-row">
+            <img src="{{ asset('./img/RAM1.jpg') }}" alt="RAM" class="block h-2/4 w-2/4 md:h-1/4 md:w-1/4 mx-auto">
+            <div class="flex flex-col">
+                <h1 class="text-2xl md:text-4xl font-bold">HyperX FURY DDR4 RGB</h1>
+                <div class="mt-5 flex flex-row justify-start">
+                    <div class="mr-5 flex flex-row">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-green-500" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                          </svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-green-500" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                          </svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-green-500" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                          </svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-green-500" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                          </svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-green-500" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                          </svg>
+                    </div>
+                    <p>5 reviews</p>
+                </div>
+                <div class="mt-5 flex flex-row justify-start">
+                    <p class="mr-5">Availability:</p>
+                    <p class="font-bold">10 pieces left</p>
+                </div>
+                <h1 class="mt-5 text-2xl md:text-4xl font-bold text-yellow-600">&#8369; 2,448.00</h1>
+            </div>
+        </div>
+        {{-- For product's overview --}}
+        <div class="p-5 w-4/5 border-b border-gray-400">
+            <h1 class="text-center font-bold">Overview</h1>
+            <div class="mt-3 flex flex-col justify-start">
+                <h1 class="mb-5 font-bold">Product's specification</h1>
+                <ul class="list-disc">
+                    <li class="text-gray-600">Test</li>
+                    <li class="text-gray-600">Test</li>
+                    <li class="text-gray-600">Test</li>
+                    <li class="text-gray-600">Test</li>
+                </ul>
+            </div>  
+        </div>
+    </div>  
+
+</x-normal_user>    
