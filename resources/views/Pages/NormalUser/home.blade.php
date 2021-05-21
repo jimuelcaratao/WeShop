25990f663c2cbbfb407c8ba2c0003b21672bd4d6
Gabriel Cacayan <gabrielcacayan@gmail.com>
2021-05-21 17:25:31 +0800

    Updated carousel UI

--- resources/views/Pages/NormalUser/home.blade.php
+++ resources/views/Pages/NormalUser/home.blade.php
@@ -1,16 +1,19 @@
 <x-normal_user>
 
-   @livewire('carousel.landing')
+   <div class="flex flex-col space-y-5">
+      @livewire('carousel.landing')
 
-   <div class="px-12">
-      <h1 class="text-center text-purple-900 text-3xl md:text-6xl my-5">Best Sellers</h1>
-
-      @livewire('carousel.products')
-   </div>
-   <div class="px-12">
-      <h1 class="text-center text-purple-900 text-3xl md:text-6xl my-5">Most Viewed</h1>
-
-      @livewire('carousel.most-viewed')
+      <div class="px-12">
+         <h1 class="text-center text-purple-900 text-3xl md:text-5xl my-5">Best Sellers</h1>
+         @livewire('carousel.products')
+      </div>
+      <br>
+      <br>
+      <br>
+      <div class="px-12">
+         <h1 class="text-center text-purple-900 text-3xl md:text-5xl my-5">Most Viewed</h1>
+         @livewire('carousel.most-viewed')
+      </div>
    </div>
 
 </x-normal_user>
