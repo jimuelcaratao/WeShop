66bd1aa5722c254e2f8090cfdcbd0366f503b503
jimuelcaratao <caratao.jimuel@gmail.com>
2021-06-05 10:45:09 +0800

    modifying sorting

--- resources/views/livewire/catalog/sorting.blade.php
+++ resources/views/livewire/catalog/sorting.blade.php
@@ -11,30 +11,30 @@
           {{-- product categories content --}}
            <div class="p-3 uppercase border-b-2 border-gray-500" id="product-categories-content">
               <div class="flex flex-col px-5">
-                  <a href="#" class="flex flex-row hover:underline py-1"><svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
-                  </svg>Peripherals</a>
-                  <a href="#" class="flex flex-row hover:underline py-1"><svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
-                  </svg>Components</a>
-                  <a href="#" class="flex flex-row hover:underline py-1"><svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
-                  </svg>Mobile devices</a>
-                  <a href="#" class="flex flex-row hover:underline py-1"><svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
-                  </svg>Branded pc</a>
-                  <a href="#" class="flex flex-row hover:underline py-1"><svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
-                  </svg>wearables</a>
-                  <a href="#" class="flex flex-row hover:underline py-1"><svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
-                  </svg>gaming console</a>
-                  <a href="#" class="flex flex-row hover:underline py-1"><svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
-                  </svg>others</a>
-                  <a href="#" class="flex flex-row hover:underline py-1"><svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
-                  </svg>bundle program</a>
+                  <a href="#" class="flex flex-row hover:underline py-1">
+                     <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
+                     </svg>
+                     Peripherals
+                  </a>
+                  <a href="#" class="flex flex-row hover:underline py-1">
+                     <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
+                     </svg>
+                     Components
+                  </a>
+                  <a href="#" class="flex flex-row hover:underline py-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
+                     </svg>
+                     Mobile devices
+                  </a>
+                  <a href="#" class="flex flex-row hover:underline py-1">
+                     <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
+                     </svg>
+                     Pre-Build
+                  </a>
               </div>
            </div>  {{-- product categories content --}}
 
